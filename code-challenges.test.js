0e8f6a98355e68c84f6ca084123afb91ba14c86e
JICaddick <james_caddick16@hotmail.com>
2022-07-11 12:49:30 -0500

    Completed Assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,35 +15,93 @@
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+describe("removeAndShuffle", () => {
+  const colors1 = ["purple", "blue", "green", "yellow", "pink"];
+  const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquama  rine", "saffron"];
+  it("a function that takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
+    const actualShuffle = removeAndShuffle(colors1);
+    expect(actualShuffle.sort()).toEqual(colors1.sort());
+    it("a function that takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
+      const actualShuffle = removeAndShuffle(colors2);
+      expect(actualShuffle.sort()).toEqual(colors2.sort());
+    })
+  })
+})
+
+  // b) Create the function that makes the test pass.
+
+// pseudo code:
+// Create a function (removeAndShuffle) that takes in an array as a param. 
+// Use .shift to remove the first item form the array
+// Write another variable to store the variable length
+// Write another variable to shuffle the data. Store the randomised position in 'i'
+// Store the shuffled data and return array
+
+  
+  const removeAndShuffle = (array) => {
+    array.shift()
+    let = arrayLength = array.length, t, i;
+    while (arrayLength) {
+      i = Math.floor(Math.random() * arrayLength--);
+      t = array[arrayLength];
+      array[arrayLength] = array[i];
+      array[i] = t;
+    }
+    return array;
+  }
 
 
-// b) Create the function that makes the test pass.
+
+  // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
+
+  // a) Create a test with expect statements for each of the variables provided.
+  const nums1 = [3, 56, 90, -8, 0, 23, 6]
+  const nums2 = [109, 5, 9, 67, 8, 24]
+    
+  describe("minAndMaxNums", () => {
+    it("a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.", () => {
+      expect(minAndMaxNums(nums1)).toEqual([-8, 90])
+      expect(minAndMaxNums(nums2)).toEqual([5, 109])
+    })
+  })
+
+  // b) Create the function that makes the test pass.
+
+// pseudo code:
+// Write a statement and give it 'array' as a param.
+// Return the minimum and maximum values by using math.min and math.max
+// The Spread operator will return just the 2 select values thus achieveing the ends. 
+
+  let minAndMaxNums = (array) => {
+    return [Math.min(...array), Math.max(...array)]
+  }
+
+  // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 
-// --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
+  // a) Create a test with an expect statement using the variables provided.
 
-// a) Create a test with expect statements for each of the variables provided.
+  describe("noDuplicates", () => {
+    const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+    const testArray2 =  [7, 8, 2, 3, 1, 5, 4]
+    it("a function that takes in two arrays as arguments and returns one array with no duplicate values.", () => {
+      const actualNoDuplicates = noDuplicates(testArray1, testArray2)
+      expect(actualNoDuplicates).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
+    })
+  })
 
-const nums1 = [3, 56, 90, -8, 0, 23, 6]
-// Expected output: [-8, 90]
-const nums2 = [109, 5, 9, 67, 8, 24]
-// Expected output: [5, 109]
+  // b) Create the function that makes the test pass.
 
+  // pseudo code:
+// Write a function (noDuplicates) that takes in both arrays as params. 
+// store new array in a let variable (new array) and set it to equal the 2 arrays concatenated. 
+// return new array without variables using the spread operator and the new Set keyword. 
 
-// b) Create the function that makes the test pass.
+  const noDuplicates = (array1, array2) => {
+    let newArray = array1.concat(array2)
+    return newArray = [...new Set(newArray)]
+  }
 
-
-// --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
-
-// a) Create a test with an expect statement using the variables provided.
-
-const testArray1 = [3, 7, 10, 5, 4, 3, 3]
-const testArray2 = [7, 8, 2, 3, 1, 5, 4]
-// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
-
-
-// b) Create the function that makes the test pass.
+console.log(noDuplicates(testArray1, testArray2))
+  
+// I couldn't get the test on this one to pass. 
